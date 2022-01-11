4c874199d167cb2931212ba99880fd1e13938b28
Brandon Rock <sandbrock@byui.edu>
2022-01-10 20:55:45 -0700

    Finished team activity 2

--- routes/ta02.js
+++ routes/ta02.js
@@ -1,15 +1,50 @@
-//TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
 
+const users = [
+  {
+    name: 'Jim Carry'
+  },
+  {
+    name: 'Bob Hope'
+  },
+  {
+    name: 'Scarlett Johansson'
+  }
+];
+
+let errorMsg = '';
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    path: '/ta02',
+    users: users,
+    errorMsg: errorMsg
   });
+  errorMsg = '';
+});
+
+router.post('/addUser', (req, res, next) => {
+  const { name } = req.body;
+  const userIndex = users.findIndex((user) => user.name === name);
+  if (userIndex < 0) {
+    users.push({ name: name });
+  } else {
+    errorMsg = `User ${name} already exists.`;
+  }
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const { name } = req.body;
+  const userIndex = users.findIndex((user) => user.name === name);
+  if (userIndex >= 0) {
+    users.splice(userIndex, 1);
+  } else {
+    errorMsg = `User ${name} does not exist.`;
+  }
+  res.redirect('/ta02');
 });
 
 module.exports = router;
